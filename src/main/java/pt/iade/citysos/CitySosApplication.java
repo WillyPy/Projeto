03b8e3756208b3796f2d1ae8be9fbf0d2da7956b
William Valter Ambrósio Feijó <50035088@iade.pt>
2020-11-14 17:45:47 +0000

    Criação da pasta do projeto local

---
+++ src/main/java/pt/iade/citysos/CitySosApplication.java
@@ -0,0 +1,13 @@
+package pt.iade.citysos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitySosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitySosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pt/iade/citysos/CitySosApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.iade.citysos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitySosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
