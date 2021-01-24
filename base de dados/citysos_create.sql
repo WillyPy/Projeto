5f923a3ead425a78d5b348655607a4af9c5475b4
William Valter Ambrósio Feijó <50035088@iade.pt>
2021-01-24 21:54:30 +0000

    Database

--- base de dados/citysos_create.sql
+++ base de dados/citysos_create.sql
@@ -6,30 +6,44 @@ create table empresa (e_id int not null auto_increment,
 						e_nome varchar(30) not null,
                         e_regiao varchar(30) not null,
                         primary key (e_id));
-create table pedido (p_id int not null auto_increment,
-						ped_tipo varchar(30) not null,
-						ped_cart_sol varchar(30) not null,
-						pedido_e_id int not null,
-						pedido_ong_id int not null,
-                        pedido_tip_ped_id int not null,
-						primary key (p_id));
-create table ong(ong_id int not null auto_increment,
-				ong_nome varchar(30) not null,
-                ong_e_id int not null,
-                primary key (ong_id));
 create table tipo_pedido(tip_ped_id int not null auto_increment,
 						tip_nome varchar(30),
                         primary key (tip_ped_id));
+create table estado_pedido(est_id int not null,
+							est_estado varchar(30),
+                            primary key(est_id));
+create table ong(ong_id int not null auto_increment,
+				ong_nome varchar(30) not null,
+                ong_e_id int not null unique, #unique para criar uma ligação de um para um com a tabela Empresa.
+                primary key (ong_id));
+create table pedido (p_id int not null auto_increment,
+						p_tipo varchar(30) not null,
+						p_cart_sol varchar(150) not null,
+						p_ong_id int not null,
+                        p_tip_ped_id int not null,
+						primary key (p_id));
+create table empresa_pedido(ep_id int auto_increment not null,
+							ep_data date not null,
+                            ep_e_id int not null,
+                            ep_p_id int not null,
+                            ep_est_id int not null,
+                            primary key(ep_id));
 #Chaves estrangeiras
-alter table pedido add constraint pedido_fk_empresa
-			foreign key (pedido_e_id) references empresa(e_id)
-            on delete no action on update no action;
-alter table pedido add constraint pedido_fk_ong
-			foreign key (pedido_ong_id) references ong(ong_id)
-            on delete no action on update no action;
-alter table pedido add constraint pedido_fk_tipo_pedido
-			foreign key (pedido_tip_ped_id) references tipo_pedido(tip_ped_id)
-            on delete no action on update no action;
 alter table ong add constraint ong_fk_empresa
 			foreign key (ong_e_id) references empresa(e_id)
+            on delete no action on update no action;
+alter table pedido add constraint pedido_fk_ong
+			foreign key (p_ong_id) references ong(ong_id)
+            on delete no action on update no action;
+alter table pedido add constraint pedido_fk_tipo_pedido
+			foreign key (p_tip_ped_id) references tipo_pedido(tip_ped_id)
+            on delete no action on update no action;
+alter table empresa_pedido add constraint empresa_pedido_fk_empresa
+			foreign key (ep_e_id) references empresa(e_id)
+            on delete no action on update no action;
+alter table empresa_pedido add constraint empresa_pedido_fk_pedido
+			foreign key (ep_p_id) references pedido(p_id)
+            on delete no action on update no action;
+alter table empresa_pedido add constraint empresa_pedido_fk_estado_pedido
+			foreign key (ep_est_id) references estado_pedido(est_id)
             on delete no action on update no action;
--- base de dados/citysos_populate.sql
+++ base de dados/citysos_populate.sql
@@ -1,7 +1,7 @@
-insert into empresa (e_id, e_nome, e_regiao) values (1,'Ajuda Lda','Zango');
-
-insert into ong (ong_id, ong_nome, ong_e_id) values (1,'Ajudante Lda',1);
-
-insert into tipo_pedido (tip_ped_id, tip_nome) values (1,'Construção');
-
-insert into pedido (p_id, ped_tipo, ped_cart_sol, pedido_e_id, pedido_ong_id, pedido_tip_ped_id) values ('example','example',1,1,1);
+insert into empresa (e_nome, e_regiao) values ('Ajuda Lda','Zango');
+insert into estado_pedido(est_id, est_estado) values(0,'SUBMETIDO');
+insert into ong (ong_nome, ong_e_id) values ('Ajudante Lda',1);
+insert into tipo_pedido (tip_nome) values ('Construção');
+insert into pedido (p_tipo, p_cart_sol, p_ong_id, p_tip_ped_id) values ('example','example',1,1);
+insert into empresa_pedido(ep_data, ep_e_id, ep_p_id, ep_est_id) values(str_to_date('2020.1.01','%Y.%m.%d'),1,1,0);
+commit;
